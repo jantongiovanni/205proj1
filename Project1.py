4168bf45668f841f5d0d6b7f6e653992fce1db3e
Joe Anotngiovanni <jantongiovanni@csumb.edu>
2017-02-02 18:56:08 +0000

    First commit

---
+++ Project1.py
@@ -0,0 +1,12 @@
+#print "Hello World"
+
+from PIL import Image
+image = Image.open('1.png')
+width, height = image.size
+print width
+print height
+pixel_values = list(image.getdata())
+#return pixel_values
+
+pic = image.load()
+print pic[0,0]
